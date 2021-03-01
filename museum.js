a04ab3e3fc118926b170afa968de544c504fb6e5
merwanjdly <79838415+merwanjdly@users.noreply.github.com>
2021-03-01 11:07:14 +0100

    Add files via upload

---
+++ museum.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $( "a" ).click(function() {
+        $( "#video" ).animate({
+            right: '+=1000px',
+            height: '+=1150px',
+            width: '+=150px'
+            
+              
+
+
+          });
+        }); 
+        
+  
+  });
